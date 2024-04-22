925b8be74a65d465f4da464eafcf2d3d4c6db7c9
Ed <alpezed@gmail.com>
2024-04-22 18:42:45 +0800

    Adding carousel on query layout

--- build/blocks/filters/index.asset.php
+++ build/blocks/filters/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a22050f4c7b3a46445f7');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'd0c05332594646ff814c');
